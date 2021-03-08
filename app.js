4f250dd7b5c4f49c7746bb6fbde41728c9937b7c
waleedfarraj <waleed.farraj95@gmail.com>
2021-03-08 23:51:22 +0200

    ability to delete a wrong order

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 'use strict'
 var arr = []
+var deliveryFee  
+var orignalTotal = orignalTotal || 0
 var derTisch = document.getElementById('result')
 let table = document.createElement('table')
+let tbod = document.createElement('tbody')
 function Order(name,item,qt,price){
 this.name = name
 this.item = item
@@ -14,61 +17,91 @@ arr.push(this)
 form.addEventListener('submit',ordering)
 function ordering(event){
   event.preventDefault()
-  var orignalTotal = orignalTotal || 0
-let deliveryFee = event.target.fee.value
+  
+ deliveryFee = event.target.fee.value
 let hungryPerson = event.target.name.value
 let food = event.target.order.value
 let quantity = event.target.one.value
 let price = event.target.money.value
 new Order(hungryPerson,food,quantity,price)
-for(var x = 0 ; x < arr.length ; x++){
-  
-orignalTotal += arr[x].price
-}
-console.log('orignalTotal',orignalTotal)
-var deliveryCost = deliveryFee / arr.length
-console.log('deliveryCost Per Person ;',deliveryCost)
-let results = []
+let deliveryCost = doMath(deliveryFee)
 
-for(var x = 0 ; x < arr.length ; x++){
-  let y  = arr[x].name + '=' + ( parseFloat(arr[x].price) + parseFloat(deliveryCost))
-results.push(y)
-}
 
-console.log(results)
 
-table.innerHTML=""
 
 firstRowMaker()
-contant(deliveryCost)
+content(deliveryCost)
 
 } 
 
 function firstRowMaker(){
+    table.innerHTML=""
   let row = document.createElement('tr')
   let firstCell = document.createElement('th')
   firstCell.textContent='Name'
   let seconedCell = document.createElement('th')
   seconedCell.textContent='Share'
+  let thirdCell = document.createElement('th')
+  thirdCell.textContent = 'OOPS!'
   row.appendChild(firstCell)
   row.appendChild(seconedCell)
+  row.appendChild(thirdCell)
 table.appendChild(row)
 derTisch.appendChild(table)
 }
-function contant(deliveryCost){
-  let tbod = document.createElement('tbody')
+function content(deliveryCost){
+  tbod.innerHTML=''
 for(var x = 0 ; x < arr.length ; x++){
   let row = document.createElement('tr')
+  row.id = x
   let first  = arr[x].name  
-  let sec = ( parseFloat(arr[x].price) + parseFloat(deliveryCost))
+  let price = arr[x].price || 0
+  let sec = ( parseFloat(price) + parseFloat(deliveryCost))
     let firstCell = document.createElement('td')
   firstCell.textContent=first
   let seconedCell = document.createElement('td')
   seconedCell.textContent=sec
+  let thirdCell = document.createElement('td')
+  let btn = document.createElement('button')
+  btn.innerHTML = 'Cancel'
  row.appendChild(firstCell)
-  row.appendChild(seconedCell)
-tbod.appendChild(row)
+ row.appendChild(seconedCell)
+ row.appendChild(thirdCell)
+ thirdCell.appendChild(btn)
+ btn.onclick = removeOrder
+ tbod.appendChild(row)
 }
 table.appendChild(tbod)
 }
+function doMath(deliveryFee){
+    for(var x = 0 ; x < arr.length ; x++){
+  
+        orignalTotal += arr[x].price
+        }
+        console.log('orignalTotal',orignalTotal)
+       var  deliveryCost = deliveryFee / arr.length || 0
+        console.log(arr.length)
+        console.log('deliveryCost Per Person ;',deliveryCost)
+        let results = []
+        console.log(deliveryCost)
+        for(var x = 0 ; x < arr.length ; x++){
+            console.log(arr[x].price)
+            console.log(deliveryCost)
+          let y  = arr[x].name + '=' + ( parseFloat(arr[x].price) + parseFloat(deliveryCost))
+        results.push(y)
+        }
+        console.log(results)
+        console.log({deliveryCost})
+        return deliveryCost
+}
 
+function removeOrder(x){
+    console.log('working')
+    let rowId = x.path[2].id
+    console.log(rowId)
+    arr.splice(rowId,1)
+    tbod.deleteRow(rowId)
+     let deliveryCost= doMath(deliveryFee)
+    firstRowMaker()
+    content(deliveryCost)
+}
