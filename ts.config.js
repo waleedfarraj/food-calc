f09b0cbedbb619ed410c6ecfddf898cb5d32086c
waleedfarraj <64354755+waleedfarraj@users.noreply.github.com>
2025-03-01 19:23:39 +0100

    Create tsconfig.js

---
+++ ts.config.js
@@ -0,0 +1,16 @@
+{
+  "compilerOptions": {
+    "target": "ES2020",
+    "module": "ES2020",
+    "moduleResolution": "node",
+    "strict": true,
+    "esModuleInterop": true,
+    "forceConsistentCasingInFileNames": true,
+    "skipLibCheck": true,
+    "outDir": "./dist",
+    "sourceMap": true,
+    "lib": ["DOM", "ES2020"]
+  },
+  "include": ["src/**/*.ts"],
+  "exclude": ["node_modules"]
+}
